1e5e6bbd1b93b8a3b90fac4a963afe1e550d9e90
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2024-12-27 12:06:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "google-cloud-bigquery",
-        "pandas"
+        'google-cloud-bigquery>=3.0.0',
+        'pandas>=1.0.0',
+        'importlib-metadata>=4.0.0',
     ],
     author="Jonathan Govinda Medrano Salazar",
     description="Librería para consumir datos de BigQuery",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/arithgrey/bq_consumer",
+    url="https://dev.azure.com/findepdev/Arquitectura/_git/bq_consumer",
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
