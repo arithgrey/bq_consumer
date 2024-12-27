df3ac95b5423370cd6b12ad63d48b871bfa6422e
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2024-12-27 14:36:00 -0600

    Update query_executor.py

--- bq_consumer/query_executor.py
+++ bq_consumer/query_executor.py
@@ -1,31 +1,36 @@
+import re
 from bq_consumer.bigquery_connector import BigQueryConnector
 from bq_consumer.config_loader import ConfigLoader
 import pandas as pd
 
 class QueryExecutor:
     def __init__(self):
-        # Carga la configuración automáticamente al crear la instancia
+        # Cargar la configuración automáticamente al crear la instancia
         config_loader = ConfigLoader()
         self.config = config_loader.get_config()
 
     def execute(self, query: str) -> pd.DataFrame:
-        # Validamos si la consulta es permitida según el universo de configuraciones
-        if not self._is_valid_query(query):
-            raise ValueError("Query not allowed in this environment")
-        
-        # Conexión a BigQuery
+        self._is_valid_query(query)
         project_id = self.config.get("project_id", "")
         connector = BigQueryConnector(project_id)
         
-        # Ejecutamos la consulta
+        # Ejecutar la consulta
         return connector.execute_query(query)
 
-    def _is_valid_query(self, query: str) -> bool:
+    def _is_valid_query(self, query: str):
         """
-        Validamos si la consulta está permitida según la configuración del archivo.
+        Valida que la consulta sea un SELECT y que no contenga comandos no permitidos como UPDATE, DELETE, etc.
         """
+        # Validar que la consulta sea un SELECT
+        if not query.lstrip().startswith("SELECT"):
+            raise ValueError("Consulta no permitida: solo se permiten consultas SELECT")
+
+        # Validación para asegurarse de que no se utilicen comandos peligrosos (UPDATE, DELETE, etc.)
+        invalid_commands = r"\b(update|delete|insert|alter|drop)\b"
+        if re.search(invalid_commands, query, re.IGNORECASE):  # Usamos re.IGNORECASE para no preocuparnos por mayúsculas/minúsculas
+            raise ValueError("Consulta no permitida: comandos como UPDATE, DELETE, INSERT, ALTER, DROP no están permitidos")
+
+        # Validación de la tabla consultada en allowed_universes
         allowed_universes = self.config.get("allowed_universes", [])
-        for universe in allowed_universes:
-            if universe in query:
-                return True
-        return False
+        if not any(universe in query for universe in allowed_universes):
+            raise ValueError("Consulta no permitida: la tabla consultada no está en el universo permitido")
