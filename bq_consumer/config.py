ecee92be5924fb9af4e4074aafbf6d7f3b09c333
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-01-09 14:58:25 -0600

    Update config.py

--- bq_consumer/config.py
+++ bq_consumer/config.py
@@ -1,6 +1,7 @@
 CONFIG = {
     "project_id": "findep-calidad-uat-mx",
     "allowed_universes": [
-        "findep-calidad-uat-mx.MICROSERVICIOS.serv_caja_unica_acceso_cajas"
+        "findep-calidad-uat-mx.MICROSERVICIOS.serv_caja_unica_acceso_cajas",
+        "ws-Buro-Clientes.ws-Buro-Clientes.draft_test"
     ]
 }
